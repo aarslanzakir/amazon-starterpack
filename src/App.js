79c3b76c62066e8db6f31e2826f955ed112bb615
aarslanzakir <aliarslanzakir@gmail.com>
2023-01-12 11:40:21 +0500

    Amazon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
+import Hero from "./Hero/Hero";
+import Header from "./components/header/Header";
+import Slider from "./components/Slider/Slider.jsx";
+import Virtual from "./components/Virtual/Virtual";
+import Products from "./components/Products/Products";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <span>Subscribe Zainkeepscode</span>
-    </div>
+  <div className="App">
+    <Header  />
+    <Hero />
+    <Slider />
+    <Virtual />
+    <Products />
+    <Testimonials />
+    <Footer/>
+  </div>
   );
 }
 
 export default App;
+
